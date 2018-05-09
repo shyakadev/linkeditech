e84d30c34f45130723f8a65437d0d8c704375c5d
shyakadev <shyakadev@gmail.com>
2018-05-09 10:38:51 +0200

    contact form modification

---
+++ js/contact_form.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+	$('#contactForm').on('submit',function(e){
+		e.preventDefault();
+
+		var name = $('#name').val();
+		var _replyto = $('#replyto').val();
+		var message = $('#message').val();
+
+		$.ajax({
+			url:'https://formspree.io/shyakadev@gmail.com',
+			method: 'POST',
+			data: {
+				name:name,
+				_replyto:_replyto,
+				message: message,
+				_subject: 'Contact form Submission',
+			},
+			dataType: "json",
+			success:function(){
+				console.log('success');
+				$('thanks').show();
+			}
+		})
+	})
+})
